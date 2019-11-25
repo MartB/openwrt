a1f6b15214d8a935094cebbd83a46c26b72844e0
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-11-25 16:08:31 +0100

    kernel: add support for kernel 5.4 (WIP)
    - Only x86
    - Symbols still needs filtering
    - no crashlog support yet as a required file got deleted upstream
    - Removed patch below, which is now seen as a recursive dependency [1]
    - Removed patch below due to build error [2]
    - fix still required to avoid identical function def [3]
    - Fixes included from Blocktrron
    
    [1] target/linux/generic/hack-5.4/260-crypto_test_dependencies.patch
    [2] target/linux/generic/hack-5.4/207-disable-modorder.patch
    [3] target/linux/generic/pending-5.4/613-netfilter_optional_tcp_window_check.patch
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -9,10 +9,12 @@ endif
 LINUX_VERSION-4.9 = .206
 LINUX_VERSION-4.14 = .158
 LINUX_VERSION-4.19 = .88
+LINUX_VERSION-5.4 = .2
 
 LINUX_KERNEL_HASH-4.9.206 = 1df27899a9bc4dd874f3830ab9e7b638f0026dffd67a2e230444e5d2805ccad6
 LINUX_KERNEL_HASH-4.14.158 = 0d49ec72e273f6fc7462ad56849b2285f13fd5f4185e8f932a0074fc2eca1bb3
 LINUX_KERNEL_HASH-4.19.88 = c1923b6bd166e6dd07be860c15f59e8273aaa8692bc2a1fce1d31b826b9b3fbe
+LINUX_KERNEL_HASH-5.4.2 = 32f98256877ca6b016715ffffcf184f1603df9e17a324787f252cd602e03a557
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
